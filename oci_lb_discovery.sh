b163e6042d6ddb45c3c1dc7e056e2c1bae655afb
Walker Garcia <walkerdgp@gmail.com>
2026-04-02 11:55:50 -0300

    Suppress Python warnings in oci_lb_discovery.sh
    suprimir a saída de erros (onde o alerta do Python é gerado) adicionando 2>/dev/null logo antes do pipe (|) que envia os dados para o jq

--- oci_lb_discovery.sh
+++ oci_lb_discovery.sh
@@ -4,18 +4,18 @@ export SUPPRESS_LABEL_WARNING=True
 
 # Variaveis de ambiente e caminhos absolutos
 CONFIG_FILE="/home/zabbix/.oci/config"
-OCI_BIN="/usr/local/bin/oci"  # <-- Substitua pelo resultado do 'which oci'
-JQ_BIN="/usr/bin/jq"          # <-- Substitua pelo resultado do 'which jq'
+OCI_BIN="/usr/local/bin/oci"
+JQ_BIN="/usr/bin/jq"
 
-# Executa a busca com caminho absoluto
+# Executa a busca suprimindo os alertas do Python (2>/dev/null)
 $OCI_BIN search resource structured-search \
   --config-file "$CONFIG_FILE" \
   --query-text "query loadbalancer resources" \
-  --output json | $JQ_BIN '
+  --output json 2>/dev/null | $JQ_BIN '
   [
     .data.items[] | {
       "{#HOSTNAME}": ."display-name" | ascii_upcase,
       "{#OCID}": .identifier,
       "{#ENVIRONMENT}": (if (."display-name" | ascii_downcase | startswith("bcoi")) then "Homologacao" else "Producao" end)
     }
-  ]' 
+  ]'
